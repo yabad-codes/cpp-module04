453946adf5a8abf918c2b313c2ab75ef75ecfd01
Yahya ABAD <yabad@student.1337.ma>
2023-09-24 16:25:52 +0100

    ICharacter class provided

---
+++ ex03/ICharacter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/24 13:54:36 by yabad             #+#    #+#             */
+/*   Updated: 2023/09/24 16:24:07 by yabad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+# include <iostream>
+# include "AMateria.hpp"
+
+class ICharacter {
+	public:
+		virtual ~ICharacter();
+		virtual std::string const& getName() const = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif
